d618a0eb89743734ca61a5a16fe884a0d94052eb
million-art <millionmulugeta09@gmail.com>
2025-03-20 15:45:19 +0300

    init

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+# config/settings.py
+FIREBASE_CONFIG = {
+    "apiKey": "AIzaSyBuyjls6eY2HUfOk2cyUrIrjXaBaZk5VJM",
+    "authDomain": "tradex-98c00.firebaseapp.com",
+    "projectId": "tradex-98c00",
+    "storageBucket": "tradex-98c00.appspot.com",
+    "messagingSenderId": "1009697941836",
+    "appId": "1:1009697941836:web:db7e0502baedfdc83187e9",
+    "measurementId": "G-PM28S3DGM4"
+}
